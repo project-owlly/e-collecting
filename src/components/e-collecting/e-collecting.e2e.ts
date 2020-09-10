addf85d104802d2060e8bafb586dc277b26f4946
peterpeterparker <david.dalbusco@outlook.com>
2020-09-10 15:32:54 +0200

    test: button enabled with mock data

--- src/components/e-collecting/e-collecting.e2e.ts
+++ src/components/e-collecting/e-collecting.e2e.ts
@@ -1,21 +1,4 @@
 import {newE2EPage} from '@stencil/core/testing';
-import {Owlly} from '../types/owlly';
-
-const owllyMock: Partial<Owlly> = {
-  title: 'Sichere Velorouten für Zürich',
-  description: 'Mehr als die Hälfte der Velofahrenden gibt an, dass sie sich im Strassenverkehr häufig unsicher fühlen.',
-  link: `/initiative/sichere-velorouten-fuer-zuerich`,
-  organisation: 'Initiativkomitee «Sichere Velorouten für Zürich»',
-};
-
-jest.mock('../helpers/utils', () => ({
-  initOwlly: async (id): Promise<Owlly | undefined> => {
-    return {
-      id: `${id}`,
-      ...owllyMock,
-    } as Owlly;
-  },
-}));
 
 describe('e-collecting', () => {
   it('renders', async () => {
@@ -32,15 +15,5 @@ describe('e-collecting', () => {
     await page.setContent('<e-collecting>Hello World</e-collecting>');
     const component = await page.find('e-collecting');
     expect(component.textContent).toEqual(`Hello World`);
-  });
-
-  it('renders an enabled button', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<e-collecting id="28c72f42-5daa-4a28-b39d-b51b203c3740"></e-collecting>');
-    const button = await page.find('e-collecting >>> button');
-
-    expect(button).not.toBe(null);
-    expect(button).not.toHaveAttribute('disabled');
   });
 });
--- src/components/e-collecting/e-collecting.spec.ts
+++ src/components/e-collecting/e-collecting.spec.ts
@@ -1,5 +1,25 @@
 import {newSpecPage} from '@stencil/core/testing';
 
+import {Owlly} from '../types/owlly';
+
+const owllyMock: Partial<Owlly> = {
+  title: 'Sichere Velorouten für Zürich',
+  description: 'Mehr als die Hälfte der Velofahrenden gibt an, dass sie sich im Strassenverkehr häufig unsicher fühlen.',
+  link: `/initiative/sichere-velorouten-fuer-zuerich`,
+  organisation: 'Initiativkomitee «Sichere Velorouten für Zürich»',
+};
+
+jest.mock('../helpers/utils', () => ({
+  initOwlly: async (id): Promise<Owlly | undefined> => {
+    return id
+      ? ({
+          id: `${id}`,
+          ...owllyMock,
+        } as Owlly)
+      : undefined;
+  },
+}));
+
 import {ECollecting} from './e-collecting';
 
 describe('e-collecting', () => {
@@ -32,6 +52,25 @@ describe('e-collecting', () => {
           </button>
         </mock:shadow-root>
         Hello World
+      </e-collecting>
+    `);
+  });
+
+  it('renders an enabled button', async () => {
+    const owllyId: string = '28c72f42-5daa-4a28-b39d-b51b203c3740';
+
+    const {root} = await newSpecPage({
+      components: [ECollecting],
+      html: `<e-collecting owlly-id="${owllyId}"></e-collecting>`,
+    });
+    expect(root).toEqualHtml(`
+      <e-collecting owlly-id="${owllyId}">
+        <mock:shadow-root>
+          <button>
+            <slot>e-Collecting</slot>
+          </button>
+          <a href="https://owly.ch${owllyMock.link}" rel="noopener noreferrer" target="_blank"></a>
+        </mock:shadow-root>
       </e-collecting>
     `);
   });
