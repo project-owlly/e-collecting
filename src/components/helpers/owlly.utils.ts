2e54ed272a38e45d447450bae3b247d3e314af24
peterpeterparker <david.dalbusco@outlook.com>
2020-10-03 14:02:07 +0200

    feat: load and use google font lato

--- src/components/helpers/owlly.utils.ts
+++ src/components/helpers/owlly.utils.ts
@@ -27,3 +27,28 @@ export const loadOwlly = async (id: string): Promise<Owlly | undefined> => {
     return undefined;
   }
 };
+
+export function injectCSS(id: string, src: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    if (!document) {
+      resolve();
+      return;
+    }
+
+    if (document.getElementById(id)) {
+      resolve('CSS already loaded.');
+      return;
+    }
+
+    const link = document.createElement('link');
+    link.id = id;
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', src);
+
+    link.addEventListener('load', () => resolve('CSS loaded.'));
+
+    link.addEventListener('error', () => reject('Error loading css.'));
+    link.addEventListener('abort', () => reject('CSS loading aborted.'));
+    document.head.appendChild(link);
+  });
+}
