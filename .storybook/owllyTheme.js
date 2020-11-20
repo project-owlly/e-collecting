21341eb486bfc03a790fd6292968925a39687ef9
peterpeterparker <david.dalbusco@outlook.com>
2020-11-20 14:41:32 +0100

    feat: init owlly theme

---
+++ .storybook/owllyTheme.js
@@ -0,0 +1,9 @@
+import {create} from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Owlly',
+  brandUrl: 'https://owlly.ch',
+  brandImage: 'https://owlly.ch/assets/logo/owlly-light.png',
+});
